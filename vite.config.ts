e26ecc25a0d7ae9cb41aeaaf292f00b0bf4ab55d
prince-at <princelokko1@gmail.com>
2025-12-01 09:52:14 +0000

    Comment out unused path import and resolve alias configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
    base: '/Akroma-Nyame-Consult',
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')}],
-    },
+ 
   });
+
+
+
+  //  resolve: {
+  //   alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')}],
+  //   },
