d14c3995e02eb8f753ff981290b7fe5bdb86ae77
prince-at <princelokko1@gmail.com>
2025-12-01 11:42:11 +0000

    Update repository URLs in package.json and refine Vite config alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import path from 'path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
    base: '/Akromah-Nyame-Travel-and-Tour/',
+     resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')}],
+    },
  
   });
 
