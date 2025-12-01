8566e4529ece0163c86a476deb442c49085e4d04
prince-at <princelokko1@gmail.com>
2025-12-01 09:28:47 +0000

    Update deployment directory to 'dist' and adjust homepage URL in package.json; set base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Akroma-Nyame-Consult',
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, 'src')}],
     },
